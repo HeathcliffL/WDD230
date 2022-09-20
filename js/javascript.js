e865d14be4be60670dde0965bc707bd8d0044c88
HeathcliffL <49813952+HeathcliffL@users.noreply.github.com>
2022-09-20 15:22:47 -0600

    creat js and css files

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+//document.getElementById("lastupdated").textContent = document.lastModified;
+const lastmodified = new Date(document.lastModified);
+document.getElementById("lastupdated").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()}/${lastmodified.getFullYear()}`;
