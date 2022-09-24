f0e89ba10452a0f5315916258054656b4fc0c62f
HeathcliffL <HeathcliffL@users.noreply.github.com>
2022-09-24 17:21:35 -0600

    lesson 2

--- lesson2/js/javascript.js
+++ lesson2/js/javascript.js
@@ -1,5 +1,3 @@
 const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
-//document.getElementById("lastupdated").textContent = document.lastModified;
-const lastmodified = new Date(document.lastModified);
-document.getElementById("lastupdated").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()}/${lastmodified.getFullYear()}`;
+document.getElementById("lastupdated").textContent = document.lastModified;
